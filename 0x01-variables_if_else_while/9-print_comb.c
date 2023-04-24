428fa103a0e9813b6136ba347fe0f372dff55318
gracy90 <gracywiredu@gmail.com>
2023-04-24 13:52:24 -0700

    9 print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<time.h>
+
+/**
+ *main - Entry point
+ *Return: Always 0(Success)
+ */
+int main(void)
+{
+    int a;
+    while (a < 0)
+    {
+        putchar(48 + a);
+        if (a != 9)
+        {
+            putchar(',');
+            putchar(' ');
+        }
+        a++;
+    }
+
+    putchar('\n');
+    return (0);   
+}
