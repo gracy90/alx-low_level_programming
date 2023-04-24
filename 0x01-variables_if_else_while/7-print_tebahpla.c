61efa1512562413c2f4636a0be947942f696805d
gracy90 <gracywiredu@gmail.com>
2023-04-24 11:29:47 -0700

    smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -10,10 +10,10 @@
 int main(void)
 {
 	char low;
+	
+	for (low = 'z'; low >= 'a'; low--)
+	putchar(low);
+	putchar('\n');
 
- 	for (low = 'z'; low >= 'a'; low--)
- 	putchar(low);
- 	putchar('\n');
-
- 	return (0);
+	return(0);
 }
