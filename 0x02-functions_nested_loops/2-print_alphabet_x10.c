dd2aa2acaee573dc0e1e9d0b7b9a8af30945aa17
gracy90 <gracywiredu@gmail.com>
2023-04-26 05:05:36 -0700

    changes 10x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- *main - Entry point
+ *print_alphabet_x10 - prints number
  *Return: Always 0(Success)
  */
 
 void print_alphabet_x10(void)
 {
 	char c;
-	int i =0;
+	int i = 0;
 
-	while (i <=0)
+	while (i <= 9)
 	{
 	for (c = 'a'; c <= 'z'; c++)
 	{
