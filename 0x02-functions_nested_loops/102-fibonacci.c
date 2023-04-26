824fcae51302e64332189a8e62cbee570039e0a8
gracy90 <gracywiredu@gmail.com>
2023-04-26 05:51:08 -0700

    first 100 fibanocci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Return: nothing
+ */
+
+int main(voif)
+{
+	int i = 0;
+	long j = i, k =2;
+
+	while (i < 50)
+	{
+	if (i == 0)
+	printf("%ld", j);
+	else if (i == 1)
+	printf(", %ld", k);
+	else
+	{
+	k += j;
+	j = k - j;
+	printf(", %ld", k);
+	}
+	++i;
+	}
+	printf("\n");
+	return 0;
