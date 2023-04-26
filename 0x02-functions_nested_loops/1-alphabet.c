45d8baa996f4926c60e93323192831af2f92a72b
gracy90 <gracywiredu@gmail.com>
2023-04-26 04:39:10 -0700

    changes to 1 alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- *main - Entry point
+ *print_alphabet - Make alphabet
  *Return: Always 0(Success)
  */
 
 void print_alphabet(void)
 {
 	char c;
+
 	for (c = '0'; c <= 'z'; c++)
 	{
 	_putchar(c);
