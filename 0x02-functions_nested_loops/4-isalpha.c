47c9ec91c13b81a89c271c2bd884d0becfa3b90c
gracy90 <gracywiredu@gmail.com>
2023-04-26 05:22:39 -0700

    changes to isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- *main - Entry point
+ *_isalpha - Entry point
+ *@c: character to be checked
  *Return: Always 0(Success)
  */
 
 int _isalpha(int c)
 {
-	if ((c >= 65 && c <=98) || (c >= 97 && c <=122))
+	if ((c >= 65 && c <= 98) || (c >= 97 && c <= 122))
 	{
 	return (1);
 	}
