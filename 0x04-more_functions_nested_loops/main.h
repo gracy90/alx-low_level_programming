bb1e4ffb09023cd24ad02ee97f61d6f639321f98
gracy90 <gracywiredu@gmail.com>
2023-04-28 04:59:42 -0700

    is upper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,25 +1,23 @@
 int _putchar(char c);
 
-void print_alphabet(void);
+int _isupper(int c);
 
-void print_alphabet_x10(void);
+int _isdigit(int c);
 
-int _islower(int c);
+int null(int a, int b);
 
-int _isalpha(int c);
+void print_numbers(void);
 
-int _print_sign(int n);
+void print_most_numbers(void);
 
-int _abs(int);
+void more_members(void);
 
-int print_last_digit(int);
+void print_line(int n);
 
-void jack_bauer(void);
+void print_diagonal(int n);
 
-void times_table(void);
+void print_square(int size);
 
-int add(int, int);
+void print_triangle(int size);
 
-void print_to_98(int n);
-
-void print_times_table(int n);
+void print_number(int n);
