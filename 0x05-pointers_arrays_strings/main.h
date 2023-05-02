0282a4c992145731493b8559ed1ba156c7597932
gracy90 <gracywiredu@gmail.com>
2023-05-02 05:45:26 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,23 @@
+int _putchar(char c);
+
+void reset_to_98(int *n);
+
+void swp_int(int *a, int *b);
+
+int _strlen(char *s);
+
+void _puts(char *str);
+
+void print_rev(char *s);
+
+void rev_string(char *s);
+
+void puts2(char *str);
+
+void puts_half(char *str);
+
+void print_arry(int *a, int n);
+
+char *_strcpyl(cha *dest, char *src);
+
+int _atoil(char *s);
