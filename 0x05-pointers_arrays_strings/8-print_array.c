4f2b2fbc6de45caa656ba688016f20d09d12c377
gracy90 <gracywiredu@gmail.com>
2023-05-02 10:33:04 -0700

    changes to 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,14 +3,14 @@
 
 
 /**
- * print_arry - prints elements of an array
+ * print_array - prints elements of an array
  * @a: array of integers
  * @n: number of elements of teh array
  * Return: void
  */
 
 void print_array(int *a, int n)
-/* print_array: prints elements of an array*/
+/* print_array: prints n elements of an array of integers*/
 {
 	int j;
 
