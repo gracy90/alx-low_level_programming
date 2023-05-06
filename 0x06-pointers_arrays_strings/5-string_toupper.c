09a4dc946b6b8a716fb2142af14bdbf66403feb3
gracy90 <gracywiredu@gmail.com>
2023-05-05 18:18:30 -0700

    5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,12 +10,12 @@ char *string_toupper(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while (str[i])
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
 		{
-			str[i] = str[i] - 32;
+			str[i] -=  32;
 		}
 		i++;
 	}
-	return (str)i;
+	return (str);
