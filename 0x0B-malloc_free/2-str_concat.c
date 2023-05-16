e1e37ee32ee86c38776c422c1ca58126c1c877db
gracy90 <gracywiredu@gmail.com>
2023-05-16 08:36:50 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,7 +16,7 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
-		s2 ="";
+		s2 = "";
 	while (s1[i])
 		i++;
 	while (s2[j])
@@ -25,7 +25,7 @@ char *str_concat(char *s1, char *s2)
 	s = malloc((sizeof(char) * l) + 1);
 
 	if (s == NULL)
-		return(NULL);
+		return (NULL);
 	j = 0;
 
 	while (k < l)
@@ -42,4 +42,3 @@ char *str_concat(char *s1, char *s2)
 	s[k] = '\0';
 	return (s);
 }
-
