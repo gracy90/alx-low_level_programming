e694254b44b8beb2d6a6492be62ebbcc98d1aac7
gracy90 <-gracywiredu@gmail.com>
2023-06-06 12:02:56 -0700

    changes to six

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -3,7 +3,7 @@
 /**
  * get_nodeint_at_index - returns the node at a certain index in a linked list
  * @head: first node in the linked list
- * @index: index of the node 
+ * @index: index of the node to return
  * Return: pointer to the node looking for
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
@@ -16,6 +16,5 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		tep = tep->next;
 		j++;
 	}
-
 	return (tep ? tep : NULL);
 }
