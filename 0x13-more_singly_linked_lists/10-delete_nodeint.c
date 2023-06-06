536b5f5856bda6ccf14a631960af2e229519c476
gracy90 <-gracywiredu@gmail.com>
2023-06-06 12:24:48 -0700

    changes to 9

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -30,7 +30,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		cpy = cpy->next;
 	}
 
-	tmp = cpy->next;
+	tep = cpy->next;
 	cpy->next = tep->next;
 	free(tep);
 	return (1);
